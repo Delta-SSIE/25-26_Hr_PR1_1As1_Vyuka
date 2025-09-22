d28ccf03cfcd808b718a8eae3a3ba4b443bed2e7
VeruHorena <horenovska.veronika@gmail.com>
2025-09-22 12:52:49 +0200

    hodina 22_9

---
+++ AhojSvete/AhojSvete/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AhojSvete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej své jméno:");
+            string uzivatel = Console.ReadLine();
+            Console.WriteLine("Ahoj " + uzivatel);
+        }
+    }
+    
+}
---
+++ Uvodni_hodina/Uvodni_hodina/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Uvodni_hodina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   String slovo1;
+               String slovo2;
+               Console.WriteLine("Zadej 1. slovo");
+               slovo1 = Console.ReadLine();
+               Console.WriteLine("Zadej 2.slovo");
+               slovo2 = Console.ReadLine();
+               Console.WriteLine("Slovo 1 bylo: " + slovo1 + " Slovo 2 bylo: " + slovo2);
+            */
+            int a;
+            int b;
+            int vysledek;
+            Console.WriteLine("Zadej prvni hodnotu:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej druhou hodnotu:");
+            b = int.Parse(Console.ReadLine());
+            vysledek = a + b;
+            Console.WriteLine("Vysledek souctu je: " + vysledek);
+
+        }
+    }
+}
