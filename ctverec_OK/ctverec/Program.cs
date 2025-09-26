d4b46664a53aaba3431fc1f602e525acb5448919
VeruHorena <horenovska.veronika@gmail.com>
2025-09-26 12:08:57 +0200

    hodina 26_9

---
+++ ctverec_OK/ctverec/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        // Sem přijde váš kód
+        int a;
+        Console.WriteLine("Zadej mi stranu");
+        a = int.Parse(Console.ReadLine());
+        int obsah = a * a;
+        Console.WriteLine("Vysledek je: " + obsah);
+
+
+        // Konec prostoru pro kód
+    }
+}
