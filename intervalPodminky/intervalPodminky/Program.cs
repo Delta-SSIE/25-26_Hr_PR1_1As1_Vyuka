783c62a9b849975d2938d16075d911fd50efcd8e
VeruHorena <horenovska.veronika@gmail.com>
2025-10-06 12:46:04 +0200

    intervalPodminky

---
+++ intervalPodminky/intervalPodminky/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace intervalPodminky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej hodnotu:");
+            int hodnota = int.Parse(Console.ReadLine());
+            if((hodnota>=0) && (hodnota <= 20))
+            {
+                if (hodnota <= 10)
+                    Console.WriteLine("Hodnota je v 1.intervalu");
+                else
+                    Console.WriteLine("Hodnota je ve 2. intervalu");
+            }
+            else
+            {
+                Console.WriteLine("Zadána nesprávná hodnota.");
+            }
+        }
+    }
+}
