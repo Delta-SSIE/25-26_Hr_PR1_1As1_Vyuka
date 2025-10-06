2d9afdea39660076b09a9acd1bc3b2e02beef95a
VeruHorena <horenovska.veronika@gmail.com>
2025-10-06 12:32:57 +0200

    podminky

---
+++ OdmocninaPodminiky/OdmocninaPodminiky/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OdmocninaPodminiky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vysledek;
+            Console.WriteLine("Zadej mi hodnotu k odmocnění");
+            int hodnota = int.Parse(Console.ReadLine());
+            if (hodnota > 0)
+            {
+                vysledek = Math.Sqrt(hodnota);
+                Console.WriteLine("Odmocnina je: " + vysledek);
+            }
+            else
+            {
+                Console.WriteLine("Nelze odmocnit.");
+            }
+        }
+    }
+}
