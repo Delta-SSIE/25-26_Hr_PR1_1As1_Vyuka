3443a426c4fe5909aa22c49f9eb24b8d9f3afd45
VeruHorena <horenovska.veronika@gmail.com>
2025-10-13 12:33:48 +0200

    kvadranty

---
+++ kvadranty - ok/kvadranty/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int x, y;
+        Console.WriteLine("Zadej souradnici x: ");
+        x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Zadej souradnici y: ");
+        y = int.Parse(Console.ReadLine());
+
+        /*
+         * if ((x > 0) && (y > 0))
+            Console.WriteLine("Bod je v 1. kvadrantu.");
+        if ((x < 0) && (y > 0))
+            Console.WriteLine("Bod je v 2. kvadrantu.");
+        if ((x < 0) && (y < 0))
+            Console.WriteLine("Bod je v 3. kvadrantu.");
+        if ((x > 0) && (y < 0))
+            Console.WriteLine("Bod je v 4. kvadrantu.");
+        if ((x == 0) || (y == 0))
+            Console.WriteLine("Bod je na ose.");
+
+        */
+        if ((x > 0) && (y > 0))
+            Console.WriteLine("Bod je v 1. kvadrantu.");
+        else if ((x < 0) && (y > 0))
+            Console.WriteLine("Bod je v 2. kvadrantu.");
+        else if ((x < 0) && (y < 0))
+            Console.WriteLine("Bod je v 3. kvadrantu.");
+        else if ((x > 0) && (y < 0))
+            Console.WriteLine("Bod je v 4. kvadrantu.");
+        else
+            Console.WriteLine("Lezi na ose.");
+
+
+
+    }
+}
